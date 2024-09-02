55d30e46edc17c5043c9766579f47bb2fcb102c8
Tuan T. Pham <tuan@vt.edu>
2024-09-01 22:34:45 -0700

    Add example config for services provided by Piped Piper
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- scripts/gen_env.sh
+++ scripts/gen_env.sh
@@ -2,4 +2,5 @@
 # Load default environment variables
 
 source .env
-cat traefik/traefik.yml.tmpl | envsubst > traefik/traefik.yml  
+cat template/traefik.yml.tmpl | envsubst > traefik/traefik.yml
+#cat template/traefik-pipedpiper.yml.tmpl | envsubst > traefik/traefik-pipedpiper.yml
